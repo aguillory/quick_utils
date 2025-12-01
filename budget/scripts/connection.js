e4398b82eabd8dba62d037e9670c987d73ee6b32
Alyssa McMurray-Guillory <mcmurray@ulm.edu>
2025-12-01 16:47:51 -0600

    Updates, bug fixes, new tab (budget items)

--- budget/scripts/connection.js
+++ budget/scripts/connection.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
-// --- PASTE YOUR FIREBASE CONFIGURATION OBJECT HERE ---
 const firebaseConfig = {
     apiKey: "AIzaSyBUNb26WpRCPUJN3k51X_A9OndQFZwICjY",
     authDomain: "budget-87a6d.firebaseapp.com",
@@ -10,13 +9,7 @@ const firebaseConfig = {
     messagingSenderId: "1040095975333",
     appId: "1:1040095975333:web:8401747beb9e7e5ebd7a4c"
 };
-// ---------------------------------------------------------
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 const auth = getAuth(app);
-
-// Export the db instance to be used in other modules
 export { db, auth };
